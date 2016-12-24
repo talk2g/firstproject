a414983f65178863e654d385b5301760d0b26f22
talk2g <gaurav.deopa2016@gmail.com>
2016-12-24 20:15:36 +0530

    added substraction feature in calculator

--- calc.cpp
+++ calc.cpp
@@ -5,7 +5,17 @@ void main()
 int a,b,c;
 printf("\n enter the numbers");
 scanf("%d%d",&a,&b);
-c=a+b;
+printf("\n 1) addition
+           2) subtraction
+);
+scanf("%d",&d);
+switch(d)
+{
+case 1: c=a+b;
 printf("\n the sum is %d ",c);
+case 2: c=a-b;
+printf("\n the sum is %d ",c);
+
+}
 getch();
 }
